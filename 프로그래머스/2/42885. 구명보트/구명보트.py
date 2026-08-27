09a5aafe04b770c133d9a8d4997de365d23cbe8b
donggwon <307701kk@gmail.com>
2026-08-27 16:08:06 +0900

    [level 2] Title: 구명보트, Time: 7.60 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,13 @@
 def solution(people, limit):
     answer = 0
-    start,end = 0,len(people)-1
-    people  = sorted(people)
+    people.sort()
+    start = 0
+    end = len(people) - 1
     
-    while start<=end :
-        little,fat = people[start],people[end]
-        if little+fat>limit:
-            end -=1
-            answer +=1
-        else:
-            start+=1
-            end-=1
-            answer +=1
-
+    while start <= end:
+        if people[start] + people[end] <= limit:
+            start += 1
+        end -= 1
+        answer += 1
+        
     return answer
