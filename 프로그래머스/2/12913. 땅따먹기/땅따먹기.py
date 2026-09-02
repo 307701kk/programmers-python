22bf8a628deb01b8eb294c37ee855002a3d25cda
donggwon <307701kk@gmail.com>
2026-09-02 18:50:48 +0900

    [level 2] Title: 땅따먹기, Time: 149.47 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,8 +1,6 @@
 def solution(land):
-    for i in range(len(land)):
-        if i == 0:
-            continue
-        else:
-            for j in range(4):
-                land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+            
     return max(land[-1])
